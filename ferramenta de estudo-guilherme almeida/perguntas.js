345a83394bfd35591ec474be49e6f270ea8314db
GuilhermeAlmeida46 <00001117566687sp@al.educacao.sp.gov.br>
2024-11-18 22:30:44 -0300

    Add files via upload

---
+++ ferramenta de estudo-guilherme almeida/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Português',
+    'o que linguagem Ciéntifica'
+    'O  padrão de escrita que os autores de textos científicos se utilizam para poder transmitir informações científicas.'
+)
+
+criaCartao(
+    'Geografia',
+    'o Que linha asiatica?',
+    'grandes cadeias montanhosas, extensos planaltos e depressões, além de dispor de climas contrastantes.'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma Programação?',
+    ' é o processo de criação de um conjunto de instruções que dizem ao computador como realizar uma tarefa. '
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'o que lingua inglesa ?',
+    'é uma língua germânica ocidental '
+)
